dd687daec1526d9065f842e6992c39431656e900
BimaEkaSetiawan <bimaekasetiawan1010@gmail.com>
2025-05-20 15:45:28 +0700

    membuat constructor berparameter

--- latihan.cpp
+++ latihan.cpp
@@ -2,5 +2,18 @@
 #include <string>
 using namespace std;
 
+class Barang{
+    public:
+    Barang(string namaBarang, int kodeBarang);
+};
 
+Barang::Barang(string namaBarang, int kodeBarang){
+    cout << "constructor berparameter" << endl;
+    cout << "nama barang = "<< namaBarang << endl;
+    cout << "kode barang =" << kodeBarang << endl;
+}
+
+int main(){
+    Barang ipin("asep", 23333);
+    return 0;
 }
